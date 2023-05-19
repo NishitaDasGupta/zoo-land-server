8f4676b50e9dabcee3502ada4e2555da398d6115
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-05-19 08:44:04 +0600

    install express cors dotenv mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${DB_USER}:${DB_KEYS}@cluster0.gvng5am.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEYS}@cluster0.gvng5am.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
