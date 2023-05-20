e525ac19c5b321e32c2fa7e44bbdfad683130454
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-05-20 06:49:55 +0600

    single toy added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -41,6 +41,15 @@ async function run() {
             res.send(result);
         })
 
+
+        app.get('/alltoy/:id', async (req,res)=> {
+            const id = req.params.id;
+            console.log(id);
+            const query = {_id: new ObjectId(id)};
+            const result = await allToysCollection.findOne(query);
+            console.log("Find Toy ", id);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
